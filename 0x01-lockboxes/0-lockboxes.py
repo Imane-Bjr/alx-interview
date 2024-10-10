111f504cd8800d3ae30942645b6ab588c75dc132
Imane-Bjr <boujirimane1@gmail.com>
2024-10-10 21:11:35 +0000

    Fixig the task error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Unlock all boxes by collecting keys from each accessible box."""
+"""Unlock boxes by collecting keys from each accessible box."""
 
 
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-    Start with the key to box 0, then collect keys from accessible boxes.
-    Continue unlocking boxes with the collected keys until no more boxes can be unlocked.
+    Start with the key to box 0, 
+    then collect keys from accessible boxes.
+    Continue unlocking boxes with the collected 
+    keys until no more boxes can be unlocked.
     Return True if all boxes are unlocked, otherwise False.
     """
     boxes_tot = len(boxes)
